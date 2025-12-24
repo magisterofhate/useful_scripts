a2b2ad0c0ead01a415053738415c341e6600b99b
Magister_of_Hate <magisterofhate@gmail.com>
2025-12-24 12:45:56 +0300

    debug mode off

--- yt_timesheet/app.py
+++ yt_timesheet/app.py
@@ -163,4 +163,4 @@ def index():
 
 if __name__ == "__main__":
     # для простоты — debug=True; на проде убрать
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=False)
